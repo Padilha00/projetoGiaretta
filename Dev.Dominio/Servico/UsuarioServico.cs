80e506985d690ebe1eb270c6ffea7916e48fc84e
Padilha00 <estermotapadilha@gmail.com>
2025-09-24 19:34:09 -0300

    Ultimas alterações

--- Dev.Dominio/Servico/UsuarioServico.cs
+++ Dev.Dominio/Servico/UsuarioServico.cs
@@ -4,36 +4,54 @@ using Dev.Dominio.modelos;
 namespace Dev.Dominio.Servico
 
 {
-    public class UsuarioServico (IUsuarioRepositorio _usuarioRepositorio): IUsuario
+    public class UsuarioServico (IUsuarioRepositorio _usuarioRepositorio): IUsuario //DI
     {
-        public async Task Adicionar(Usuario usuario)
+        public async Task Adicionar(Usuario usuario) //parametro
         {
-            if (usuario is null)
-                throw new Exception("Usuario é null");
+           if (usuario is null)
+
+                throw new Exception("O usuario nao pode ser nulo");
 
             await _usuarioRepositorio.Adicionar(usuario);
+        }
+
+        public async Task Atualizar(Usuario usuario)
+        {
+            if (usuario is null)
+
+                throw new Exception("O usuario nao pode ser nulo");
+
+            var usuarioDb = await _usuarioRepositorio.LerPorId(usuario.Id);
+
+            usuarioDb.Nome = usuario.Nome;
+            usuarioDb.CPF = usuario.CPF;
+            usuarioDb.Email = usuario.Email;
+            usuarioDb.Telefone = usuario.Telefone;
+            usuarioDb.Senha = usuario.Senha;
+            usuarioDb.ConfirmarSenha = usuario.ConfirmarSenha;
+
+            await _usuarioRepositorio.Atualizar(usuarioDb);
+        }
+
+        public async Task<IList<Usuario>> Ler()
+        {
+           return await _usuarioRepositorio.Ler();
+        }
+
+        public async Task<Usuario> LerPorId(int id)
+        {
+           return await _usuarioRepositorio.LerPorId(id);
+        }
+
+        public async Task Remover(int id)
+        {
+            var usuarioDb = await _usuarioRepositorio.LerPorId(id);
+
+            if (usuarioDb is null)
+
+                throw new Exception("O usuario nao pode ser nulo");
                 
-            //validar se a pessoa esta ok e salvar no banco. 
-        }
-
-        public Task Atualizar(Usuario usuario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IList<Usuario>> Ler()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Usuario> LerPorId(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Remover(int id)
-        {
-            throw new NotImplementedException();
+            await _usuarioRepositorio.Remover(id);
         }
     }
 
--- Dev.Dominio/obj/Debug/net9.0/Dev.Dominio.AssemblyInfo.cs
+++ Dev.Dominio/obj/Debug/net9.0/Dev.Dominio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dev.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7b5666f5aa987602e17a7edfbdaa6d45d5d824")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dev.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dev.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
